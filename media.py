7a7144dda051d12a886675c13a9d8df2b1d49825
hemanthmarukala <hemanth.arike@gmail.com>
2017-09-26 16:17:37 -0700

    Update media.py

--- media.py
+++ media.py
@@ -2,21 +2,28 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Sep 17 17:45:02 2017
-
 @author: hemanthrahulmarukala
 """
 import webbrowser
 """
--Movie class to assign the instance variables for the objects that will be included in the entertainment_center.py
--show_trailer function to how the youtube video of the movie trailer. 
+-Movie class to assign the instance variables for the objects
+-that will be included in the entertainment_center.py
+-show_trailer function to how the youtube video of the movie trailer.
 """
+
+
 class Movie():
-    # Class constructor to initilise the instance variables
-    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+    def __init__(
+        self,
+        movie_title,
+        movie_storyline,
+        movie_poster,
+        movie_trailer
+    ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.trailer_youtube_url = movie_trailer
         self.poster_image_url = movie_poster
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
